0c489de4fcfc430a4051ff9f2afcc36a6d60f37d
jugg3rnautt <63144393+jugg3rnautt@users.noreply.github.com>
2020-11-14 08:50:12 +0530

    Create Add Two Numbers.java

---
+++ Add Two Numbers.java
@@ -0,0 +1,94 @@
+//You are given two non-empty linked lists representing two non-negative integer
+//s. The digits are stored in reverse order, and each of their nodes contains a si
+//ngle digit. Add the two numbers and return the sum as a linked list. 
+//
+// You may assume the two numbers do not contain any leading zero, except the nu
+//mber 0 itself. 
+//
+// 
+// Example 1: 
+//
+// 
+//Input: l1 = [2,4,3], l2 = [5,6,4]
+//Output: [7,0,8]
+//Explanation: 342 + 465 = 807.
+// 
+//
+// Example 2: 
+//
+// 
+//Input: l1 = [0], l2 = [0]
+//Output: [0]
+// 
+//
+// Example 3: 
+//
+// 
+//Input: l1 = [9,9,9,9,9,9,9], l2 = [9,9,9,9]
+//Output: [8,9,9,9,0,0,0,1]
+// 
+//
+// 
+// Constraints: 
+//
+// 
+// The number of nodes in each linked list is in the range [1, 100]. 
+// 0 <= Node.val <= 9 
+// It is guaranteed that the list represents a number that does not have leading
+// zeros. 
+// 
+// Related Topics Linked List Math 
+// 👍 9734 👎 2444
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ *
+ * @Author: Rudrakh
+ */
+class Solution {
+    /**
+     * Algorithm: Linked list traversal
+     * Space: Extra O(N)
+     * Time: O(N)
+     * @param l1
+     * @param l2
+     * @return
+     */
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode head = new ListNode();
+        ListNode ansList = new ListNode();
+        head.next = ansList;
+        int carry = 0;
+        while(l1 != null || l2 != null) {
+            ListNode newNode = new ListNode();
+            int value = carry;
+            if (l1 != null) {
+                value += l1.val;
+                l1 = l1.next;
+            }
+            if (l2 != null) {
+                value += l2.val;
+                l2 = l2.next;
+            }
+            newNode.val = value%10;
+            carry = value/10;
+            ansList.next = newNode;
+            ansList = ansList.next;
+        }
+        if (carry > 0) {
+            ListNode newNode = new ListNode(carry);
+            ansList.next = newNode;
+        }
+        return head.next.next;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
